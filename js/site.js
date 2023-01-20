a2f7e14966d0fc304c87f4b92a1946f83bb8b952
tRomanova <66643712+tRomanova@users.noreply.github.com>
2023-01-20 09:04:46 -0600

    changed code in displayResults function

--- js/site.js
+++ js/site.js
@@ -13,7 +13,7 @@ function getValues() {
     displayResults(checkedString);
 }
 // This function is responsible determining whether a string is a palindrome
-function checkForPalindrome(strToCheck, reversedString) {
+function checkForPalindrome(strToCheck) {
     const regex = /[^A-Za-z0-9]/gi;
     let newString = ""
     let palindrome = true;
@@ -27,7 +27,7 @@ function checkForPalindrome(strToCheck, reversedString) {
         if (regString[i] === regString[j]) {
             i += 1;
         } else {
-            newString = "isNotPalindrome"
+            newString = "Not Palindrome"
             palindrome = false;
         }
     }
@@ -40,18 +40,40 @@ function checkForPalindrome(strToCheck, reversedString) {
 
 //This function is responsible for displaying a message to the user to show 
 //whether their string is a palindrome or not
-function displayResults(checkedString) {
-    let className;
+function displayResults(result) {
+    let alert = document.getElementById('alert');
 
-    if (checkedString !== "isNotPalindrome") {
-        className = 'alertPalindrome';
-        document.getElementById('results').innerHTML = `<div class=${className} ><h1>Well Done! You entered a Palindrome!</h1><hr>
-            <h4>Your phrase reversed is: <b> ${checkedString} </b></h4></div>`;
-        document.getElementById('alert').classList.remove('invisible');
+    //reset alert box
+    alert.classList.remove('alert-danger');
+    alert.classList.remove('alert-success');
+
+    let messageElement = document.getElementById('message');
+    let resultElement = document.getElementById('results');
+
+    console.log("***", result);
+    if (result === "Not Palindrome") {
+        alert.classList.add('alert-danger');
+        messageElement.textContent = "Sorry! You entered NOT a palindrome!"
+        resultElement.textContent = "Try again!"
     } else {
-        className = 'alertNotPalindrom';
-        document.getElementById('results').innerHTML = `<div class=${className}><h1>Sorry! You entered not a Palindrome!</h1><hr>
-        <h4>Try again!</h4></div>`;
-        document.getElementById('alert').classList.remove('invisible');
+        alert.classList.add('alert-success');
+        messageElement.textContent = "Well Done! You entered a palindrome!"
+        resultElement.textContent = "Your string reversed is: " + result
     }
+
+    alert.classList.remove('invisible');
+
+    // let className;
+
+    // if (checkedString !== "isNotPalindrome") {
+    //     className = 'alertPalindrome';
+    //     document.getElementById('results').innerHTML = `<div class=${className} ><h1>Well Done! You entered a Palindrome!</h1><hr>
+    //         <h4>Your phrase reversed is: <b> ${checkedString} </b></h4></div>`;
+    //     document.getElementById('alert').classList.remove('invisible');
+    // } else {
+    //     className = 'alertNotPalindrom';
+    //     document.getElementById('results').innerHTML = `<div class=${className}><h1>Well Done! You entered a Palindrome!</h1><hr>
+    //     <h4>Try again!</h4></div>`;
+    //     document.getElementById('alert').classList.remove('invisible');
+    // }
 }
